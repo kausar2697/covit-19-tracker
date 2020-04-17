7a46fe7b3909b2d1c836f5d8ba3ea905bf9e83c0
kausar2697 <17103126@iubat.edu>
2020-04-17 19:15:00 +0600

    Updates

---
+++ precache-manifest.290fcc0b8cce41772237614ed0f32ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a01dc8d40ce208b00da98228b541dd",
+    "url": "/covit-19-tracker/index.html"
+  },
+  {
+    "revision": "58ba715131fc6cd4dc6a",
+    "url": "/covit-19-tracker/static/css/main.400bab8c.chunk.css"
+  },
+  {
+    "revision": "27d63be2f53c9876b5e7",
+    "url": "/covit-19-tracker/static/js/2.dae56be5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covit-19-tracker/static/js/2.dae56be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ba715131fc6cd4dc6a",
+    "url": "/covit-19-tracker/static/js/main.3b5ef85f.chunk.js"
+  },
+  {
+    "revision": "58eaaebde2016706c343",
+    "url": "/covit-19-tracker/static/js/runtime-main.269d0763.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covit-19-tracker/static/media/image.d7265326.png"
+  }
+]);
