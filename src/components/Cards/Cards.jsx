fa6ba9369206cada8b7bc1b467fce37a1eaf2fbd
kausar2697 <17103126@iubat.edu>
2020-04-18 15:22:26 +0600

    new update number add

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,8 +9,12 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   if (!confirmed) {
     return 'Loading...';
   }
-
+   var newAdd=[];
+   newAdd.push(confirmed.value);
+   var lastIndex=newAdd.length
+   console.log(newAdd)
   return (
+     
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
@@ -21,9 +25,13 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             <Typography variant="h5" component="h2">
               <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
             </Typography>
+            <Typography variant="h5" component="h2">
+              <CountUp start={0} end={(newAdd[lastIndex-1])-(newAdd[lastIndex-2])} duration={2.75} separator="," />
+            </Typography>
             <Typography color="textSecondary">
               {new Date(lastUpdate).toDateString()}
             </Typography>
+            
             <Typography variant="body2" component="p">
               Number of active cases of COVID-19.
             </Typography>
